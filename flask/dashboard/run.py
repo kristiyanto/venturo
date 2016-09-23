5b8c895757eac89b2c3442f8e82d406b218b0f79
Daniel Kristiyanto <danielkr@uw.edu>
2016-09-23 01:24:07 +0000

    flask

--- flask/dashboard/run.py
+++ flask/dashboard/run.py
@@ -1,3 +1,14 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+
+    if port == 5000:
+        app.debug = True
+
+    app.run(host='0.0.0.0', port=port)
