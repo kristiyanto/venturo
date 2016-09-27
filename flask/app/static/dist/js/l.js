60bce950addabed67ae95d022b9dd2ae1409a5d3
Daniel Kristiyanto <danielkr@uw.edu>
2016-09-26 21:48:17 -0700

    template

--- flask/app/static/dist/js/l.js
+++ flask/app/static/dist/js/l.js
@@ -9,16 +9,3 @@ L.tileLayer('https://api.mapbox.com/styles/v1/kristiyanto/citktoe5o000e2iqxokgem
     accessToken: 'pk.eyJ1Ijoia3Jpc3RpeWFudG8iLCJhIjoiY2l0a3Q1b2VmMGNuaTJubnZjOWN3NWp0dSJ9.9en-5YLbMPvG7hnoGt35XA'
 }).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var circle = L.circle([51.508, -0.11], 10, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap);
-
-var circle = L.circle([51.509, -0.10], 10, {
-    color: 'green',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap);
-
