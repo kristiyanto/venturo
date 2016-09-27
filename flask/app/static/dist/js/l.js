09e2c0d8f461afa99aed11ac4e9bd07318fa48a1
Daniel Kristiyanto <danielkr@uw.edu>
2016-09-26 20:21:54 -0700

    bootstrap added

---
+++ flask/app/static/dist/js/l.js
@@ -0,0 +1,24 @@
+
+
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/kristiyanto/citktoe5o000e2iqxokgem2ic/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3Jpc3RpeWFudG8iLCJhIjoiY2l0a3Q1b2VmMGNuaTJubnZjOWN3NWp0dSJ9.9en-5YLbMPvG7hnoGt35XA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'insight',
+    accessToken: 'pk.eyJ1Ijoia3Jpc3RpeWFudG8iLCJhIjoiY2l0a3Q1b2VmMGNuaTJubnZjOWN3NWp0dSJ9.9en-5YLbMPvG7hnoGt35XA'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var circle = L.circle([51.508, -0.11], 10, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(mymap);
+
+var circle = L.circle([51.509, -0.10], 10, {
+    color: 'green',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(mymap);
+
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
   return "Hello, World!"
+
+
