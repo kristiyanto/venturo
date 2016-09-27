e1618df3b98c7c6c7627736b83ef7aaeae321cae
Daniel Kristiyanto <danielkr@uw.edu>
2016-09-26 22:37:47 -0700

    simple ajax

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,12 +1,57 @@
 from app import app
-from flask import render_template, Flask
+from flask import render_template, jsonify
+from elasticsearch import Elasticsearch
+
+cluster = ['ip-172-31-0-107', 'ip-172-31-0-100', ' ip-172-31-0-105', 'ip-172-31-0-106']
+es = Elasticsearch(cluster, port=9200)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-    # return render_template("index.html")
+    # return "Hello, World!"
+    return render_template("index.html")
 @app.route('/map')
 def map():
     return render_template("index.html")
+
+@app.rout('/stats')
+def getstats():
+    return jsonify(activeDrivers(), activePass())
+
+def activeDrivers():
+    q = {
+    "query" : {
+        "constant_score" : {
+            "filter" : {
+                "range" : {
+                    "ctime" : {
+                        "gt"  : "now-2h"
+                    }
+                }
+            }
+        }
+    }
+    }
+
+    res = es.search(index='driver', doc_type='rolling', body=q, ignore=[404, 400])
+    return res['hits']['hits']
+
+def activePass():
+    q = {
+    "query" : {
+        "constant_score" : {
+            "filter" : {
+                "range" : {
+                    "ctime" : {
+                        "gt"  : "now-2h"
+                    }
+                }
+            }
+        }
+    }
+    }
+
+    res = es.search(index='passenger', doc_type='rolling', body=q, ignore=[404, 400])
+    return res['hits']['hits']
 
