2dd32734dc6860141a4d509cb668219d6120692b
Daniel Kristiyanto <danielkr@uw.edu>
2016-10-14 07:23:18 +0000

    Tuning: Window size

--- spark/y.py
+++ spark/y.py
@@ -30,7 +30,7 @@ from geopy.distance import vincenty, Point
 #################################################################
 
 sc = SparkContext(appName="venturo")
-ssc = StreamingContext(sc, 2)
+ssc = StreamingContext(sc, 10)
 sc.setLogLevel("WARN")    
 
 
